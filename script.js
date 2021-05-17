1088e64fe9df2eedd2f82d2e3fd2bed2da252ace
modest_aryaman09 <61847192+AryamanMishra@users.noreply.github.com>
2021-05-17 15:19:23 +0530

    Basic model complete

--- script.js
+++ script.js
@@ -1,50 +1,82 @@
-const fetch_activeData = async function(state) {
+
+let statesData = {};
+
+// const test = async function() {
+// 	const testing = await axios.get('https://api.covid19india.org/state_district_wise.json');
+// 	const statestest = testing.data;
+// 	console.log(statestest);
+// }
+
+
+const fetch_allStatesData = async function() {
+	const getstates = await axios.get('https://api.covid19india.org/state_district_wise.json');
+	statesData = getstates.data;
+	//console.log(statesData);
+}
+
+const fetch_activeData = async function() {
     try {
         let config = {headers: {Accept: 'application/json'}}
         const res = await axios.get('https://api.covid19india.org/state_district_wise.json',config);
-        let actives = 0;
-        let districts = res.data[state].districtData;
-        for (const district in districts) {
-            actives += districts[district]["active"];
-        }
-        document.getElementById('MP_Active').innerText = actives;
+		for (state in statesData) {
+			let actives = 0;
+			if (state != 'State Unassigned') {
+				let districts = res.data[state].districtData;
+				for (const district in districts) {
+					actives += districts[district]["active"];
+				}
+				document.getElementById(`${state}_Active`).innerText = actives;
+			}
+		}
     }
     catch(err) {
         console.log('Error in loading',err);
     }
 }
 
-const fetch_deceasedData = async function(state) {
+const fetch_deceasedData = async function() {
     try {
         let config = {headers: {Accept: 'application/json'}}
         const res = await axios.get('https://api.covid19india.org/state_district_wise.json',config);
-        let deaths = 0;
-        let districts = res.data[state].districtData;
-        for (const district in districts) {
-            deaths += districts[district]["deceased"];
-        }
-        document.getElementById('MP_Deaths').innerText = deaths;
+        for (state in statesData) {
+			let deaths = 0;
+			if (state != 'State Unassigned') {
+				let districts = res.data[state].districtData;
+				for (const district in districts) {
+					deaths += districts[district]["deceased"];
+				}
+				document.getElementById(`${state}_Deaths`).innerText = deaths;
+			}
+		}
     }
     catch(err) {
         console.log('Error in loading',err);
     }
 }
 
-const fetch_recoveredData = async function(state) {
+const fetch_recoveredData = async function() {
     try {
         let config = {headers: {Accept: 'application/json'}}
         const res = await axios.get('https://api.covid19india.org/state_district_wise.json',config);
-        let recoveries = 0;
-        let districts = res.data[state].districtData;
-        for (const district in districts) {
-            recoveries += districts[district]["recovered"];
-        }
-        document.getElementById('MP_Recovered').innerText = recoveries;
+        for (state in statesData) {
+			let recoveries = 0;
+			if (state != 'State Unassigned') {
+				let districts = res.data[state].districtData;
+				for (const district in districts) {
+					recoveries += districts[district]["recovered"];
+				}
+				document.getElementById(`${state}_Recovered`).innerText = recoveries;
+			}
+		}
     }
     catch(err) {
         console.log('Error in loading',err);
     }
 }
-fetch_activeData("Madhya Pradesh");
-fetch_deceasedData("Madhya Pradesh");
-fetch_recoveredData("Madhya Pradesh");
+
+
+//test();
+fetch_allStatesData();
+fetch_activeData();
+fetch_deceasedData();
+fetch_recoveredData();
